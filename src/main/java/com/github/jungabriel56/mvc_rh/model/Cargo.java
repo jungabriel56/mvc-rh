9dab30b8aeb3aefacb3387d6bcd0751bd68ea7a2
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-05 21:01:42 -0300

    adição de entidades

--- src/main/java/com/github/jungabriel56/mvc_rh/model/Cargo.java
+++ src/main/java/com/github/jungabriel56/mvc_rh/model/Cargo.java
@@ -7,18 +7,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "departamentos")
-public class Departamento implements Serializable{
+@Table(name = "cargos")
+public class Cargo implements Serializable{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
-	@Column(nullable = false, length = 60, unique = true)
+	@Column(nullable = false, length = 50, unique = true)
 	private String nome;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_departamento_fk")
+	private Departamento departamento;
 	
 	
 	public Long getId() {
